955cd0d3f5ecb438e88febe086ebf683ec2909e0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-02-28 20:31:37 +0100

    Updated the index.js file

--- src/index.js
+++ src/index.js
@@ -9,8 +9,31 @@
 class DatabaseJs
 {
 	constructor(){
-		this.create = () =>{
-			
+		this.create = (key, value) =>{
+			if(typeof key != undefined || typeof value != undefined)
+			{
+				localStorage.set(key, value);
+
+			}else{
+				throw new Error("Key or value cannot be undefined");
+			}
+		}
+
+
+		this.update = (key, value) =>{
+			this.create(key,value);
+		}
+
+
+		this.get = (key) =>{
+			if(key != undefined)
+			{
+				return localStorage.get(key);
+			}
+		}
+
+		this.destroy =  () =>{
+			localStorage.clear();
 		}
 	}
 }
